344e9f6571ab6ea166093697b137fc9c309f6d1e
Kyle Rupp <kyle.rupp838@gmail.com>
2023-05-24 21:41:07 -0700

    [WEAT-6] Create sample data sql file

---
+++ db/db_test_values.sql
@@ -0,0 +1,31 @@
+INSERT INTO `amenoi_db`.`id` VALUES ('AA.AA.AA.AA', '2023-01-01 12:45:00', '2023-01-01 12:45:00');
+INSERT INTO `amenoi_db`.`id` VALUES ('AA.AA.AA.AB', '2023-01-01 12:45:00', '2023-01-01 12:45:00');
+INSERT INTO `amenoi_db`.`id` VALUES ('AA.AA.AA.AC', '2023-01-01 12:45:00', '2023-01-01 12:45:00');
+INSERT INTO `amenoi_db`.`id` VALUES ('AA.AA.AA.AD', '2023-01-01 12:45:00', '2023-01-01 12:45:00');
+
+INSERT INTO `amenoi_db`.`location` VALUES ('AA.AA.AA.AA', 'Living');
+INSERT INTO `amenoi_db`.`location` VALUES ('AA.AA.AA.AB', 'Kitchen');
+INSERT INTO `amenoi_db`.`location` VALUES ('AA.AA.AA.AC', 'Room 1');
+INSERT INTO `amenoi_db`.`location` VALUES ('AA.AA.AA.AD', 'Room 2');
+
+INSERT INTO `amenoi_db`.`firmware` VALUES ('AA.AA.AA.AA', '0.0.0');
+INSERT INTO `amenoi_db`.`firmware` VALUES ('AA.AA.AA.AB', '0.0.0');
+INSERT INTO `amenoi_db`.`firmware` VALUES ('AA.AA.AA.AC', '0.0.0');
+INSERT INTO `amenoi_db`.`firmware` VALUES ('AA.AA.AA.AD', '0.0.0');
+
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AA', 15, 15, '2023-01-01 12:00:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AA', 16, 15, '2023-01-01 12:15:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AA', 17, 15, '2023-01-01 12:30:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AA', 18, 15, '2023-01-01 12:45:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AA', 15, 15, '2023-01-01 12:00:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AB', 16, 15, '2023-01-01 12:15:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AB', 17, 15, '2023-01-01 12:30:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AB', 18, 15, '2023-01-01 12:45:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AC', 15, 15, '2023-01-01 12:00:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AC', 16, 15, '2023-01-01 12:15:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AC', 17, 15, '2023-01-01 12:30:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AC', 18, 15, '2023-01-01 12:45:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AD', 15, 15, '2023-01-01 12:00:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AD', 16, 15, '2023-01-01 12:15:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AD', 17, 15, '2023-01-01 12:30:00')
+INSERT INTO `amenoi_db`.`data` VALUES ('AA.AA.AA.AD', 18, 15, '2023-01-01 12:45:00')
